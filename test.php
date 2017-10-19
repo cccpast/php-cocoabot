c255055a6839eaef6be442b0bb10bdbb00d74a5d
cccpast <coltpex13fix@gmail.com>
2017-10-19 23:27:54 +0900

    test

--- test.php
+++ test.php
@@ -1,10 +1,12 @@
 <?php
 require_once __DIR__ . '/weather.php';
-/**
- * 単体テスト用ファイル
- * 後々PHPUnitに移行するかも？
- */
-$weather = new Weather();
-$info = $weather->getWeather("金沢");
+require_once __DIR__ . '/constinfo.php';
+
+// $weather = new Weather();
+// $info = $weather->getWeather("金沢");
 // echo $info[0]["weather"];
 // var_dump($info);
+
+foreach (ConstInfo::HIT_WORDS as $key => $val) {
+    echo ($key + 1) . " " . $val . PHP_EOL;
+}
