3f7971ba70f25b8ed4166cf3ae6a23db136297c0
cccpast <coltpex13fix@gmail.com>
2017-10-19 23:38:10 +0900

    fix

---
+++ constinfo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 定数クラス
+ */
+class ConstInfo {
+    const ACCESS_TOKEN = '';
+    const REPLY_API = 'https://api.line.me/v2/bot/message/reply';
+    const LIVEDOOR_WHEATHER_HACKS_API = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=';
+
+    const HIT_WORDS = array(
+        "確認",
+        "検索",
+        "元気だよ",
+        "まあまあかな",
+        "天気",
+        "金沢の天気は？",
+        "東京の天気は？"
+    );
+}
